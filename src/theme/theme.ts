8af337a4b90a6abe2c68dac674ca2ee50b967d14
conghuhu <15841721425@163.com>
2022-02-11 11:26:28 +0800

    去log

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -5,10 +5,9 @@ export function setTheme(themeName: string) {
   console.log(themeConfig);
   
   for (let key in themeConfig) {
-    console.log('key', key);
-    console.log('themeConfig[key]',themeConfig[key]);
-    
-
+    // console.log('key', key);
+    // console.log('themeConfig[key]',themeConfig[key]);
+  
     document
       .getElementsByTagName("body")[0]
       .style.setProperty(`--${key}`, themeConfig[key]);
